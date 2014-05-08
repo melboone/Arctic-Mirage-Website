daf452a6eb48761a357719d8bd299ecd92afaaac
Julian Litcanu <email@julian.id.au>
2014-05-08 15:36:41 +1000

    about page modifications - text br and padding

--- about.php
+++ about.php
@@ -6,16 +6,21 @@
 </div>
 <div class="about-wrapper">
 	<div><h1>WE ARE THE ARCTIC MIRAGE</h1></div>
-		<!-- <div> -->
+		<div>
 			<p>We are passionate about design.</p>
 			<p>All sorts of design.</p>
-			<p>Our boutique studio buzzes with positive energy and thrives on our knack for delivering design solutions across a variety of platforms and industries.</p>
-			<p>We work enthusiastically and collaboratively with our clients, and we get results.</p>
-			<p>Best of all, we’re small enough to be accessible, friendly enough to be approachable, and experienced enough to have confidence in our ability.</p>
-			<p>We’re creative and talented and offer a full range of graphic design services, including branding, publication design and web design.</p>
-			<p>We also provide interior design services, including 3D artist impressions, specifications, material boards and marketing colour floorplans.</p>
+			<p>Our boutique studio buzzes with positive energy and thrives on our knack<br>
+			for delivering design solutions across a variety of platforms and industries.</p>
+			<p>We work enthusiastically and collaboratively with our clients,<br>
+			and we get results.</p>
+			<p>Best of all, we’re small enough to be accessible, friendly enough to be approachable,<br>
+			and experienced enough to have confidence in our ability.</p>
+			<p>We’re creative and talented and offer a full range of graphic design services,<br>
+			including branding, publication design and web design.</p>
+			<p>We also provide interior design services, including 3D artist impressions,<br>
+			specifications, material boards and marketing colour floorplans.</p>
 			<p>We love what we do.</p>
 			<p>And it shows.</p>
-		<!-- </div> -->
+		</div>
 </div>
 <?php include("includes/footer.php"); ?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php $description = 'Arctic Mirage'; ?>
 <?php $keywords = 'marketing, design, architecture, quality, architecture design'; ?>
 <?php $title = 'Arctic Mirage'; include("includes/header.php"); ?>
+asd
 <?php include("includes/footer.php"); ?>
