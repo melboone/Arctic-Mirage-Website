54a3387459e786b19c4a7446747cac2bce456ae9
Julian Litcanu <email@julian.id.au>
2014-05-08 12:47:59 +1000

    cleaning file, changing h1 text

--- about.php
+++ about.php
@@ -1,30 +1,21 @@
 <?php $description = 'About Arctic Mirage'; ?>
 <?php $keywords = 'marketing, design, architecture, quality, architecture design'; ?>
 <?php $title = 'Arctic Mirage | About'; include("includes/header.php"); ?>
-
 <div style="text-align:center;">
     <img src="includes/images/about-arctic-mirage.jpg">
 </div>
 <div class="about-wrapper">
-	<div><h1>Who is the<br />Arctic Mirage ?</h1></div>
+	<div><h1>WE ARE<br />THE ARCTIC MIRAGE</h1></div>
 		<div>
 			<p>We are passionate about design.</p>
-
-<p>All sorts of design.</p>
-
-<p>Our boutique studio buzzes with positive energy and thrives on our knack for delivering design solutions across a variety of platforms and industries.</p>
-
-<p>We work enthusiastically and collaboratively with our clients, and we get results.</p>
-
-<p>Best of all, we’re small enough to be accessible, friendly enough to be approachable, and experienced enough to have confidence in our ability.</p>
-
-<p>We’re creative and talented and offer a full range of graphic design services, including branding, publication design and web design.</p>
-
-<p>We also provide interior design services, including 3D artist impressions, specifications, material boards and marketing colour floorplans.</p>
-
-<p>We love what we do.</p>
-
-<p>And it shows.</p>
+			<p>All sorts of design.</p>
+			<p>Our boutique studio buzzes with positive energy and thrives on our knack for delivering design solutions across a variety of platforms and industries.</p>
+			<p>We work enthusiastically and collaboratively with our clients, and we get results.</p>
+			<p>Best of all, we’re small enough to be accessible, friendly enough to be approachable, and experienced enough to have confidence in our ability.</p>
+			<p>We’re creative and talented and offer a full range of graphic design services, including branding, publication design and web design.</p>
+			<p>We also provide interior design services, including 3D artist impressions, specifications, material boards and marketing colour floorplans.</p>
+			<p>We love what we do.</p>
+			<p>And it shows.</p>
 		</div>
 </div>
 <?php include("includes/footer.php"); ?>
